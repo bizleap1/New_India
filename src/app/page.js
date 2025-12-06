37e20c9c4471662cb2270f3852ef40d6e42667ef
bizleap1 <bizleap1@gmail.com>
2025-12-06 18:46:46 +0530

    uodate

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import AboutUsSection from "@/components/Aboutus";
 import Hero from "@/components/Hero";
 import PlansSection from "@/components/PlansSection";
 import ProductsSection from "@/components/ProductsSection";
+import AchievementsSummits from "@/components/AchievementsSummits";
 
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       <Hero/>
       <AboutUsSection/>
       <AboutShowcase/>
+      <AchievementsSummits/>
       <PlansSection/>
       <ProductsSection/>
     </>
